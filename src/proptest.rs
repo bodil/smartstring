c2b794cef951f5bb747ff96ed7ffd88b1353e895
Bodil Stokke <bodil@bodil.org>
2020-07-07 17:30:23 +0100

    Add a test for the proptest strategy.

--- src/proptest.rs
+++ src/proptest.rs
@@ -1,6 +1,7 @@
 //! `proptest` strategies (requires the `proptest` feature flag).
 
 use crate::{SmartString, SmartStringMode};
+use proptest::proptest;
 use proptest::strategy::{BoxedStrategy, Strategy};
 use proptest::string::Error;
 
@@ -14,4 +15,11 @@ where
     Mode: 'static,
 {
     proptest::string::string_regex(regex).map(|g| g.prop_map(SmartString::from).boxed())
+}
+
+proptest! {
+    #[test]
+    fn strategy(string in string_regex(".+").unwrap()) {
+        assert!(!SmartString::<crate::LazyCompact>::is_empty(&string));
+    }
 }
